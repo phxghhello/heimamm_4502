3c2e9c638a694321f5fa0ff0df9a92e4355b15b2
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-03 20:12:04 +0800

    登录验证模块 - 地址白名单

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,34 @@
 import Vue from "vue";
 // 导入路由
 import VueRouter from "vue-router";
-
+import {
+  getToken,
+  removeToken
+} from '../utils/token.js'
+import {
+  userInfo
+} from '../api/user.js'
 // 导入 组件
 import login from "../views/login/login.vue";
 import index from "../views/index/index.vue";
+import {
+  Message
+} from 'element-ui';
 
 // 注册
 Vue.use(VueRouter);
+//重写push方法, 屏蔽重复跳转错误
+//解决两次访问相同路由地址报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 // 创建路由对象
 const router = new VueRouter({
-  routes: [
-    {
-      path:'/',
-      redirect:'/login'
+  routes: [{
+      path: '/',
+      redirect: '/login'
     },
     {
       path: "/login",
@@ -27,6 +41,40 @@ const router = new VueRouter({
     }
   ]
 });
+
+//地址白名单
+const whitePaths = ['/login']
+
+router.beforeEach((to, from, next) => {
+  // 首页才需要判断token
+  if (!whitePaths.includes(to.path)) {
+    if (!getToken()) {
+      Message.error("必须登录才可以访问首页");
+      return next("/login");
+    }
+    // token正确性判断
+    userInfo().then(res=>{
+      //为什么是 0
+        if(res.data.code===0){
+            // token验证失效
+            Message.error("登录状态有误，请重新登录")
+            // 删除错误的token
+            removeToken()
+            // 跳转去登录页
+            next('/login')
+        }else{
+            // token验证成功
+            next('/index')
+        }
+    })
+  } else {
+    // 放过去
+    next();
+  }
+});
+
+
+
 
 // 暴露出去
 export default router;
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,27 +3,12 @@
 </template>
 
 <script>
-import {getToken,removeToken} from '../../utils/token.js'
-import {userInfo} from '../../api/user.js'
+
+
 
 export default {
     name:"index",
-    beforeCreate() {
-      if(!getToken()){
-          this.$message.error('必须登录才可以访问首页')
-          this.$router.push('/login')
-      }
-    },
-    created() {
-      userInfo().then(res=>{
-        window.console.log(res);
-        if (res.data.code ==206 && res.data.message == 'token参数错误') {
-          this.$message.error("不能伪造token哦");
-          removeToken();
-          this.$router.push('/login');
-        } 
-      })
-    },
+
 }
 </script>
 
